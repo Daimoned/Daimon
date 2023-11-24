d950a1baf28faacee94c3707b91870b928984a2a
Defy <61808223+De-y@users.noreply.github.com>
2023-11-24 12:38:24 -0800

    f

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  devServer: {
+    port: 3000,
+  },
+  runtimeConfig: {
+    port: 3000,
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Daimon</h1>
+    <p>Welcome to Daimon, the better Minecraft Launcher.</p>
+</template>
