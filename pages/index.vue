8245e451ec30dcfa2061609ba30e1d96b49d0415
Defy <61808223+De-y@users.noreply.github.com>
2023-11-25 20:56:05 -0800

    hmm

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,34 @@
+<script setup>
+    import 'assets/base.css'
+</script>
 <template>
-    <h1>Daimon</h1>
-    <p>Welcome to Daimon, the better Minecraft Launcher.</p>
+    <div class="container">
+        <div class="content">
+            <div class="logo">
+                <img src="~/assets/logo.png" alt="Daimon Logo" />
+            </div>
+            <div class="text">
+                <h1 class="poppins-bold">Daimon</h1>
+                <p>Welcome to Daimon, the better Minecraft Launcher.</p>
+            </div>
+        </div>
+    </div>
 </template>
+<style scoped>
+    
+    .poppins-bold {
+        font-family: "Noto-Black";
+        font-size: 100px;
+    }
+    .container .content p {
+        font-size: 25px;
+        font-family: "NotoSansJP-Regular";
+    }
+    .container {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: fit-content;
+    }
+</style>
