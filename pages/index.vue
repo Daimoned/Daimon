d2e1be7e97d88c9c5adfdb4ddcba7cfec3c332a8
Defy <61808223+De-y@users.noreply.github.com>
2023-11-26 10:09:15 -0800

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,13 @@
 <template>
     <div class="container">
         <div class="content">
-            <div class="logo">
-                <img src="~/assets/logo.png" alt="Daimon Logo" />
-            </div>
             <div class="text">
                 <h1 class="poppins-bold">Daimon</h1>
                 <p>Welcome to Daimon, the better Minecraft Launcher.</p>
+            </div>
+            <div class="links">
+                <a href="/login" class="login">Login</a>
+                <a href="/register" class="signup">Register</a>
             </div>
         </div>
     </div>
@@ -20,9 +21,12 @@
         font-family: "Noto-Black";
         font-size: 100px;
     }
+    .text {
+        text-align: center;
+    }
     .container .content p {
         font-size: 25px;
-        font-family: "NotoSansJP-Regular";
+        font-family: "Poppins-Regular";
     }
     .container {
         position: absolute;
@@ -30,5 +34,42 @@
         top: 50%;
         transform: translate(-50%, -50%);
         width: fit-content;
+    }
+    .links {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+    }
+    .links a {
+        width: 150px;
+        height: 50px;
+        color: white;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 25px;
+        font-family: "Poppins-Bold";
+        color: #000;
+        text-decoration: none;
+    }
+    .links a.login {
+        background-color: #fd6565;
+        color: white;
+
+        transition: 500ms;
+    }
+    .links a.signup {
+        background-color: rgb(0, 188, 235);
+        transition: 500ms;
+        color: white;
+
+    }
+    .links a.login:hover {
+        background-color: #ff0000;
+    }
+    .links a.signup:hover {
+        background-color: rgb(0, 150, 200);
     }
 </style>
